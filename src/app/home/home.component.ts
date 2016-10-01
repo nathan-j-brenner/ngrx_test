3434ebe49d4e06c3c043e111b8f40dbce42c14b9
nathan-j-brenner <nathan.j.brenner@gmail.com>
2016-10-01 15:51:17 -0700

    created static user message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  user: string = 'Nate';
+
+  message: string = `Welcome ${this.user}`;
+
   constructor() { }
 
   ngOnInit() {
